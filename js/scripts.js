a0598bb99aff9753b9372f07028bde799a52979a
Sosina <sosinakassa12@gmail.com>
2025-07-17 21:27:01 -0500

    Custom JavaScript Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 console.log('javascript connected!');
     
 const carousel = new bootstrap.Carousel('#homeCarousel', {
-    interval: 2000,
+    interval: 5000,
     pause: false
 })
 
 // when the pause button is clicked, pause the carousel
-const carouselPause = document.getElementById('carouselPause');
-carouselPause.addEventListener('click', function() {
-    console.log('pausing the carousel');
-    carousel.pause();
+const carouselBtton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
+
+carouselBtton.addEventListener('click', function() {
+    if(faIcon.classList.contains('fa-pause')){
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    }else{
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
+    
 })
 
 // when the play button is clicked, begin cycling through the images
