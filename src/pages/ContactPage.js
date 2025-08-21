87e60bb5878b965193af7bb4c02ab8c28ede2e3f
Sosina <sosinakassa12@gmail.com>
2025-08-20 21:28:02 -0500

    ContactForm Part 1 and 2

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,6 @@
-import {Col, Row, Container} from 'reactstrap';
+import {Col, Row, Container, Label} from 'reactstrap';
 import SubHeader from '../components/SubHeader';
+import ContactForm from '../components/ContactForm';
 
 
 const ContactPage = () => {
@@ -41,7 +42,7 @@ const ContactPage = () => {
             <hr />
             </Col>
             <Col md='10'>
-            TBD: ContactForm
+              <ContactForm />
             </Col>
         </Row>
     </Container>
