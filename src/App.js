aa7000e88c93053ccd73d3e0c9a3ab41e8789984
Sosina <sosinakassa12@gmail.com>
2025-09-03 20:20:43 -0500

    Async Partners

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import './App.css';
 import AboutPage from './pages/AboutPage';
 import { useDispatch } from 'react-redux';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
         dispatch(fetchCampsites());
+        dispatch(fetchPartners());
     }, [dispatch]);
 
   return (
--- src/features/partners/PartnerList.js
+++ src/features/partners/PartnerList.js
@@ -1,21 +1,31 @@
-import {Col} from 'reactstrap';
+import {Col, Row} from 'reactstrap';
 import Partner from './Partner';
 import { selectAllPartners } from './partnersSlice';
 import { useSelector } from 'react-redux';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
 
 const PartnerList = () => {
     const partners = useSelector(selectAllPartners);
-    return (
+
+    const isLoading = useSelector((state) => state.partners.isLoading);
+    const errMsg = useSelector((state) => state.partners.errMsg);
+    return isLoading ? (
+        <Loading />
+    ) : errMsg ? (
+        <Error errMsg={errMsg} />
+    ) : (
         <Col className='mt-4'>
-            {partners.map((partner) => {
-                return (
-                    <div className='d-flex mb-5' key={partner.id}>
-                        <Partner partner={partner}  />
-                    </div>                    
-                );
-            })}
+            <Row>
+                {partners.map((partner) => {
+                    return (
+                        <div className='d-flex mb-5' key={partner.id}>
+                            <Partner partner={partner} />
+                        </div>
+                    );
+                })}
+            </Row>
         </Col>
     );
-}
-
+};
 export default PartnerList;
